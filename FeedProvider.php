e32bf718fb07bef58280cb6256c72259e923d54e
Franz-R. Reinhart <franz@neochic.de>
2018-05-04 11:44:54 +0200

    * add debug + cache param to fb feed

--- FeedProvider.php
+++ FeedProvider.php
@@ -18,10 +18,10 @@ class FeedProvider {
         return $simplePie->get_items(0, $amount);
     }
 
-    public function getFacebookFeed($fbProfileId, $appId, $appSecret, $amount = 3) {
+    public function getFacebookFeed($fbProfileId, $appId, $appSecret, $amount = 3, $debug = false, $cache = true) {
         $cacheFile = $this->getTmpDir() . '/ncfp_facebook_feed_' . $appId . '.json';
-        if (is_file($cacheFile)) {
-            $data = json_decode($this->fileGetContentsCurl($cacheFile));
+        if ($cache && is_file($cacheFile)) {
+            $data = json_decode($this->fileGetContentsCurl($cacheFile, array(), $debug));
             if ($data->timestamp > time() - 3600) {
                 return $data->stream;
             }
@@ -33,7 +33,7 @@ class FeedProvider {
         try {
             $url = "https://graph.facebook.com/" . $fbProfileId . "/posts?". $token . "&fields=created_time,message,from,to,link,object_id,status_type,picture,full_picture";
             ini_set('user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9');
-            $facebookFeed = json_decode($this->fileGetContentsCurl($url));
+            $facebookFeed = json_decode($this->fileGetContentsCurl($url, array(), $debug));
 
             if ($facebookFeed) {
                 $facebookFeed = array_filter($facebookFeed->data, function ($e) use ($fbProfileId) {
@@ -108,13 +108,15 @@ class FeedProvider {
         return sys_get_temp_dir();
     }
 
-    protected function fileGetContentsCurl($url, $curlopt = array())
+    protected function fileGetContentsCurl($url, $curlopt = array(), $debug = false)
     {
         $ch = curl_init();
         $default_curlopt = array(
             CURLOPT_TIMEOUT => 20, //timeout after 20 secs ..
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_SSL_VERIFYPEER => $debug ? 0 : 1,
+            CURLOPT_SSL_VERIFYHOST => $debug ? 0 : 1,
             CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9'
         );
         $curlopt = array(CURLOPT_URL => $url) + $curlopt + $default_curlopt;
@@ -138,8 +140,8 @@ class FeedProvider {
                 $twig->addFunction(new \Twig_SimpleFunction('getFeed', function ($url, $amount = 3) {
                     return $this->getFeed($url, $amount);
                 }));
-                $twig->addFunction(new \Twig_SimpleFunction('getFacebookFeed', function ($fbProfileId, $appId, $appSecret, $amount = 3) {
-                    return $this->getFacebookFeed($fbProfileId, $appId, $appSecret, $amount);
+                $twig->addFunction(new \Twig_SimpleFunction('getFacebookFeed', function ($fbProfileId, $appId, $appSecret, $amount = 3, $debug = false, $cache = true) {
+                    return $this->getFacebookFeed($fbProfileId, $appId, $appSecret, $amount, $debug, $cache);
                 }));
                 return $twig;
             });
